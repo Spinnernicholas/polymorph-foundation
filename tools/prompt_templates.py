5d27473ce0b19f7be0fc7ca53da15226cca84888
Nick Spinner <spinnernicholas@gmail.com>
2025-04-24 00:03:59 -0700

    Added asset enums to ai process

--- tools/prompt_templates.py
+++ tools/prompt_templates.py
@@ -10,6 +10,13 @@ type_instructions = {
         "and the value set to the object name as a string (e.g., MyClass.DataType = \"MyClass\")."
     ),
     "alias": "Generate a luacats meta file for a Lua type alias, showing the aliased type and usage.",
+    "asset_enum": (
+        "Generate a luacats meta file for a Lua enum representing asset/resource types for use with autocomplete tools. "
+        "Each enum value should match the asset string exactly. "
+        "Include a brief description and a reference link if available. Add \"_ASSET\" to the end of the enum name. "
+        "The enum should be named after the asset type, and the values should be in uppercase. "
+        "For example, if the enum is for barrels, it should be named BARREL_ASSET and contain values like BARREL, BEER, etc."
+    ),
     # Add more types as needed
 }
 
@@ -62,6 +69,22 @@ return {ClassName}
 ---[Reference]({object page url})
 ---
 ---@alias {AliasName} {AliasedType}
+```''',
+
+    "asset_enum": '''```lua
+---@meta
+
+---
+---{Description}
+---Lua enum for asset/resource types used in the project.
+---[Reference]({object page url})
+---
+---@enum {AssetName}_ASSET
+{EnumName} = {{
+    BARREL = "BARREL",
+    BEER = "BEER",
+    -- ...more asset values...
+}}
 ```'''
 }
 
