e91d49733a935799846d38039ba6e847546170a9
Nick Spinner <spinnernicholas@gmail.com>
2025-04-24 00:11:14 -0700

    added datatype for some derived data classes

--- meta/data/GAME_CONDITION_ACCUMULATE_IN_STORAGE.lua
+++ meta/data/GAME_CONDITION_ACCUMULATE_IN_STORAGE.lua
@@ -7,11 +7,11 @@
 ---[Reference](https://www.polymorph.games/foundation/modding/api/game_condition_accumulate_in_storage)
 ---
 ---@class GAME_CONDITION_ACCUMULATE_IN_STORAGE:GAME_CONDITION
+---@field DataType "GAME_CONDITION_ACCUMULATE_IN_STORAGE"
 ---@field ResourceListToAccumulate? RESOURCE_COLLECTION_VALUE|table List of RESOURCE_QUANTITY_PAIR values or a RESOURCE_COLLECTION_VALUE value to accumulate
 
 local GAME_CONDITION_ACCUMULATE_IN_STORAGE = {}
 
-
----Class methods and properties go here
+GAME_CONDITION_ACCUMULATE_IN_STORAGE.DataType = "GAME_CONDITION_ACCUMULATE_IN_STORAGE"
 
 return GAME_CONDITION_ACCUMULATE_IN_STORAGE
--- meta/data/GAME_CONDITION_DAY_COUNT.lua
+++ meta/data/GAME_CONDITION_DAY_COUNT.lua
@@ -7,11 +7,11 @@
 ---[Reference](https://www.polymorph.games/foundation/modding/api/game_condition_day_count)
 ---
 ---@class GAME_CONDITION_DAY_COUNT:GAME_CONDITION
+---@field DataType "GAME_CONDITION_DAY_COUNT"
 ---@field DaysToComplete? integer The number of days to complete (Serialized, Savegame, default is 0)
 
 local GAME_CONDITION_DAY_COUNT = {}
 
-
----Class methods and properties go here
+GAME_CONDITION_DAY_COUNT.DataType = "GAME_CONDITION_DAY_COUNT"
 
 return GAME_CONDITION_DAY_COUNT
